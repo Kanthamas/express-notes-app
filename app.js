14192407e2e1a0bacc6ba7291ee0e928970004e7
Kanthamas <114124443+Kanthamas@users.noreply.github.com>
2025-09-04 01:32:18 +0700

    refactor app.js and server.js

---
+++ app.js
@@ -0,0 +1,47 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import userRoutes from "./src/api/v1/routes/user.routes.js";
+import noteRoutes from "./src/api/v1/routes.js";
+import { authUser } from "./src/api/v1/middlewares/authUser.js";
+import { notFoundErrorHandler } from "./src/api/v1/errors/notFoundErrorHandler.js";
+import { centralizedErrorHandler } from "./src/api/v1/errors/centalizedErrorHandler.js";
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+
+app.use(
+	cors({
+		origin: ["http://localhost:5173", "http://localhost:5174"],
+	})
+);
+
+app.use(cookieParser());
+
+if (process.env.NODE_ENV !== "production") {
+	app.use(morgan("dev"));
+} else {
+	app.use(morgan("combined"));
+}
+
+// Health check route
+app.get("/health", (req, res) => {
+	res.status(200).json({
+		error: false,
+		message: "API is healthy -- ready to serve",
+		data: null,
+	});
+});
+
+// Routes
+app.use("/", userRoutes);
+app.use("/notes", authUser, noteRoutes);
+
+// Error Handlers
+app.use(notFoundErrorHandler);
+app.use(centralizedErrorHandler);
+
+export default app;
--- server.js
+++ server.js
@@ -1,46 +1,10 @@
-import express from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import morgan from "morgan";
 import chalk from "chalk";
+import app from "./app.js";
 import { connectMongoDB } from "./src/api/v1/config/databases/mongodb.js";
-import userRoutes from "./src/api/v1/routes/user.routes.js";
-import noteRoutes from "./src/api/v1/routes.js";
-import { authUser } from "./src/api/v1/middlewares/authUser.js";
-import { notFoundErrorHandler } from "./src/api/v1/errors/notFoundErrorHandler.js";
-import { centralizedErrorHandler } from "./src/api/v1/errors/centalizedErrorHandler.js";
 
 const PORT = process.env.PORT || 4000;
 
 async function startServer() {
-	const app = express();
-
-	// Middlewares
-	app.use(express.json());
-
-	app.use(
-		cors({
-			origin: ["http://localhost:5173", "http://localhost:5174"],
-		})
-	);
-
-	app.use(cookieParser());
-
-	if (process.env.NODE_ENV !== "production") {
-		app.use(morgan("dev"));
-	} else {
-		app.use(morgan("combined"));
-	}
-
-	// Routes
-	app.use("/", userRoutes);
-	app.use("/notes", authUser, noteRoutes);
-
-	// Error Handlers
-	app.use(notFoundErrorHandler);
-
-	app.use(centralizedErrorHandler);
-
 	// Server
 	try {
 		// Connect DB first
